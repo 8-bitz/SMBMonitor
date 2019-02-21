93baad367be7de407df55c3b56227cebe979d9bd
8-bitz <46509022+8-bitz@users.noreply.github.com>
2019-02-21 18:05:11 -0500

    commit 2

--- smbMonitor.py
+++ smbMonitor.py
@@ -1,5 +1,5 @@
 from scapy.all import *
-
+#ImportError: cannot import name 'NPCAP_PATH' from 'scapy.arch.pcapdnet' (C:\Users\scott\AppData\Local\Programs\Python\Python37-32\lib\site-packages\scapy\arch\pcapdnet.py)
 def smbCheck(pkt):
     pkt.show() # debug statement
     print("******************************************************************")
