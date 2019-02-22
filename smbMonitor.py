c1928635acebaffc15056462cf950a887ed9f529
8-bitz <46509022+8-bitz@users.noreply.github.com>
2019-02-21 21:06:00 -0500

    filters added

--- smbMonitor.py
+++ smbMonitor.py
@@ -1,10 +1,15 @@
 from scapy.all import *
 #ImportError: cannot import name 'NPCAP_PATH' from 'scapy.arch.pcapdnet' (C:\Users\scott\AppData\Local\Programs\Python\Python37-32\lib\site-packages\scapy\arch\pcapdnet.py)
 def smbCheck(pkt):
-    pkt.show() # debug statement
-    print("******************************************************************")
-    print(dir(pkt))
-    print(type(pkt))
-    print("******************************************************************")
+    #pkt.show() # debug statement
+    if str(pkt[IP].dst) == "10.1.1.35":
+        print("******************************************************************")
+        #print(dir(pkt))
+        print(pkt.show())
+        print(pkt[IP].dst)
+        print(pkt[IP].proto)
+
+        #print(type(pkt))
+        print("******************************************************************")
 
 sniff(iface="Realtek PCIe GBE Family Controller", prn=smbCheck, filter="tcp", store=0)
